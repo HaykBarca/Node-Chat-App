26efb52581918e6b4df0bc6ea7dfdd4c7400311b
HaykBarca <hayk19939563@gmail.com>
2018-12-08 21:51:30 +0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.send()
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
