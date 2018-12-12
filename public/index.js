3dafca38d33737636a2775e2398abed3d5900988
HaykBarca <hayk19939563@gmail.com>
2018-12-12 22:28:01 +0400

    Added scroll bottom

--- public/index.js
+++ public/index.js
@@ -4,6 +4,24 @@ socket.on('connect', function () {
     console.log('Connected to server');
 });
 
+function scrollBottom () {
+    //selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = $('#message-template').html();
@@ -14,6 +32,7 @@ socket.on('newMessage', function (message) {
     });
     
     $('#messages').append(html);
+    scrollBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -26,6 +45,7 @@ socket.on('newLocationMessage', function (message) {
     });
     
     $('#messages').append(html);
+    scrollBottom();
 })
 
 socket.on('disconnect', function () {
